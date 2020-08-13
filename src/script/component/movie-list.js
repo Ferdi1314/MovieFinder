564a8cf7d22f73e345d74bf60c33cba98f3e69f4
Ferdinand Wijaya Susilo <swferdi@gmail.com>
2020-08-13 22:07:42 +0700

    add shadowDOM

--- src/script/component/movie-list.js
+++ src/script/component/movie-list.js
@@ -1,24 +1,41 @@
 import './movie-item.js';
  
 class MovieList extends HTMLElement {
-   set movies(movies) {
-       this._movies = movies;
-       this.render();
-   }
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
  
-   renderError(message) {
-       this.innerHTML = "";
-       this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-   }
+
+    set movies(movies) {
+        this._movies = movies;
+        this.render();
+    }
  
-   render() {
-       this.innerHTML = "";
-       this._movies.forEach(movie => {
-           const movieItemElement = document.createElement("movie-item");
-           movieItemElement.movie = movie;
-           this.appendChild(movieItemElement);
-       })
-   }
+    renderError(message) {
+        this.shadowDOM.innerHTML = `
+        <style>
+            .placeholder {
+                font-weight: lighter;
+                color: rgba(0,0,0,0.5);
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+            }
+        </style>
+        `;
+        this.shadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    }
+ 
+    render() {
+        this.shadowDOM.innerHTML = "";
+        this._movies.forEach(movie => {
+            const movieItemElement = document.createElement("movie-item");
+            movieItemElement.movie = movie;
+            this.shadowDOM.appendChild(movieItemElement);
+        })
+    }
 }
  
 customElements.define("movie-list", MovieList);
